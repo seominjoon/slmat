f49550ca031256bf54812ad04f2f5e553a67483e
Min Joon Seo <seominjoon@gmail.com>
2013-08-16 17:43:31 +0900

    organized structure, added to-do list

--- zisma.py
+++ zisma.py
@@ -11,59 +11,18 @@ import os
 import sys
 from glob import glob
 from matplotlib.widgets import Button
-from collections import Counter
 
-class wave:
-	def __init__(self, data):
-		self.data = np.array(data)
+# drawing application
+class drawer:
+	def __init__(self, topimgs, botimgs):
+		self.topimgs = topimgs
+		self.topylim, self.topxlim = self.topimgs[0].shape
 
-	# optimal translation, calculated via metric function
-	# the function is normalized by norm function around z
-	def trnsl(self, another, z, std=4, trim=0.1):
-		slen = len(self.data)
-		alen = len(another.data)
-		tlen = int(min(slen,alen)*trim) # trim length
-		sumlen = slen+alen
-		vals = np.zeros(sumlen-2*tlen)
-		for t in np.arange(tlen, sumlen-tlen):
-			init = max(t,alen)
-			end = min(t,slen)+alen
-			vals[t-tlen] = self.metric(another,init-alen,end-alen,init-t,end-t)
-		# optimal translation for another
-		am = int(np.argmin(vals))
-		dist = vals[am]
-		ot =  am + tlen - alen
-		return (ot, dist)
+		self.botimgs = botimgs
+		self.botylim, self.botxlim = self.botimgs[0].shape
 
-	# measures the distance between two waves
-	# the greater, the more different are the waves
-	def metric(self, another, si, se, ai, ae):
-		num = se-si
-		if num != ae-ai:
-			raise NameError('need to be same length')
-		sumval = 0.0
-		for ind in range(num): 
-			sumval += abs(self.data[ind+si]-another.data[ai+ind])
-		return sumval / num
-
-	# perform gaussian normalization on self.data centered at z
-	# not used for now
-	def norm(self, z, std):
-		rf = norm(loc=z, scale=std)
-		result = self.data[:]
-		for ind in range(len(self.data)):
-			result[ind] *= rf.pdf(ind-z)
-		return wave(result)
-
-class app:
-	def __init__(self, folderdir, folderdir2):
-		self.files = self.fetchFiles(folderdir)
-		self.imgs = self.fetchImages(self.files)
-		self.topylim, self.topxlim = self.imgs[0].shape
-
-		self.files2 = self.fetchFiles(folderdir2)
-		self.imgs2 = self.fetchImages(self.files2)
-		self.botylim, self.botxlim = self.imgs2[0].shape
+		# set up analyzer
+		self.anlz = analyzer(topimgs, botimgs)
 
 		# set up canvas
 		self.fig = plt.figure()
@@ -77,7 +36,7 @@ class app:
 		self.topright.set_autoscaley_on(False)
 		self.topright.set_autoscalex_on(False)
 		plt.ylim([0,255])
-		plt.xlim([0,len(self.files)])
+		plt.xlim([0,len(self.topimgs)])
 
 		#self.toptrans = self.fig.add_subplot(233)
 
@@ -91,7 +50,7 @@ class app:
 		self.botright.set_autoscaley_on(False)
 		self.botright.set_autoscalex_on(False)
 		plt.ylim([0,255])
-		plt.xlim([0,len(self.files2)])
+		plt.xlim([0,len(self.botimgs)])
 
 		#self.bottrans = self.fig.add_subplot(236)
 
@@ -113,39 +72,9 @@ class app:
 		self.botvline = None
 
 		self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
-		self.topleft.imshow(self.imgs[0])
-		self.botleft.imshow(self.imgs2[0])
+		self.topleft.imshow(self.topimgs[0])
+		self.botleft.imshow(self.botimgs[0])
 		plt.show()
-
-	def fetchFiles (self, folderdir):
-		files_list = glob(os.path.join(folderdir, '*.jpg'))
-		return sorted(files_list)
-
-	def fetchImages (self, files):
-		imgs = {}
-		for ind in range(len(files)):
-			imgs[ind] = mpimg.imread(files[ind])
-		return imgs.values()
-	
-	def pixAvg (self, pic, x, y, size):
-		val = 0
-		num = 0
-		yi = -size
-		while yi <= size:
-			xs = size - abs(yi)
-			xi = -xs
-			while xi <= xs:
-				val += pic[y+yi,x+xi]
-				num += 1
-				xi += 1
-			yi += 1
-		return val/num
-		
-	def tempSeq (self, imgs, x, y):
-		seq = {}
-		for ind in range(len(imgs)):
-			seq[ind] = self.pixAvg(imgs[ind], x, y, 2)
-		return seq.values()
 
 	def finddel(self, arr, element):
 		for ind in range(len(arr)):
@@ -162,59 +91,15 @@ class app:
 			self.movebotxy(event.xdata, event.ydata)
 		if event.inaxes == self.botright:
 			self.movebotz(event.xdata)
-
-	# function to be maximzed
-	def findmax(self):
-		maxval = 0
-		maxx = 0
-		maxy = 0
-		maxz = 0
-		for x in np.array(range(40))+self.topx-20:
-			for y in np.array(range(40))+self.topy-20:
-				seq = self.tempSeq(self.imgs2,x,y)
-				currz, currval = self.sim(self.topseq,seq)
-				if currval > maxval:
-					maxval = currval 
-					maxx = x
-					maxy = y
-					maxz = currz
-		return (maxx, maxy, maxz)
 				
 	def onok(self, event):
-		# x,y,z = self.findmax()
-		# self.movebotxy(x,y)
-		# self.movebotz(z)	
-		
-		# given z in flt, select a few random points 
-		# with variance higher than given threshold
-		# assuming no translation between the two images
-		num = 20 # make 10 random selections
-		ots = np.zeros(num)
-		for ind in range(num):
-			ots[ind] = self.randtrnsl()
-		# print ots
-		self.movebotz(self.topz-sp.stats.mode(ots)[0][0])
-
-	# perform single random selection of point
-	# and return optimal translation
-	def randtrnsl(self, xi=100, xf=400, yi=100, yf=400):
-		x = int(round(xi + np.random.rand(1)[0]*(xf-xi)))
-		y = int(round(yi + np.random.rand(1)[0]*(yf-yi)))
-		topseq = self.tempSeq(self.imgs, x, y) 
-		botseq = self.tempSeq(self.imgs2, x, y)
-		topw = wave(topseq)
-		botw = wave(botseq)
-		# print "(%d,%d)" %(x,y)
-		ot, dist = topw.trnsl(botw,self.topz)
-		return ot
+		self.movebotz(self.anlz.trnsl(self.topz))
 
 	# invoked when "Compare" button is clicked
 	def oncmp(self, event):
 		# calculates optimal translation and similitude measure
-		w1 = wave(self.topseq)
-		w2 = wave(self.botseq)
-		ot, dist = w1.trnsl(w2,self.topz)
-		self.movebotz(self.topz-ot)
+		ot, dist = self.topseq.trnsl(self.botseq,self.topz)
+		self.movebotz(ot)
 		print dist
 
 	def movetopxy(self, x, y):
@@ -224,8 +109,8 @@ class app:
 			self.finddel(self.topleft.lines, self.topmarker)
 		self.topx = round(x)
 		self.topy = round(y)
-		self.topseq = self.tempSeq(self.imgs, self.topx, self.topy)
-		self.topline = self.topright.plot(self.topseq)[0]
+		self.topseq = analyzer.tempseq(self.topimgs, self.topx, self.topy)
+		self.topline = self.topright.plot(self.topseq.data)[0]
 		self.topmarker = self.topleft.plot(x, y, '-ro')[0]
 		plt.show()
 		
@@ -236,8 +121,8 @@ class app:
 			self.finddel(self.botleft.lines, self.botmarker)
 		self.botx = round(x)
 		self.boty = round(y)
-		self.botseq = self.tempSeq(self.imgs2, self.botx, self.boty)
-		self.botline = self.botright.plot(self.botseq)[0]
+		self.botseq = analyzer.tempseq(self.botimgs, self.botx, self.boty)
+		self.botline = self.botright.plot(self.botseq.data)[0]
 		self.botmarker = self.botleft.plot(x, y, '-ro')[0]
 		plt.show()
 
@@ -246,7 +131,7 @@ class app:
 			self.finddel(self.topright.lines, self.topvline)
 		self.topz = int(round(z))
 		self.topvline = self.topright.axvline(x=self.topz)
-		self.topleft.imshow(self.imgs[self.topz])
+		self.topleft.imshow(self.topimgs[self.topz])
 		plt.show()
 
 	def movebotz(self, z):
@@ -254,9 +139,126 @@ class app:
 			self.finddel(self.botright.lines, self.botvline)
 		self.botz = int(round(z))
 		self.botvline = self.botright.axvline(x=self.botz)
-		self.botleft.imshow(self.imgs2[self.botz])
+		self.botleft.imshow(self.botimgs[self.botz])
 		plt.show()
 
-# start application with given arguments
-app(sys.argv[1], sys.argv[2])
+# descriptor for z-axis wave
+class wave:
+	def __init__(self, data):
+		self.data = np.array(data)
 
+	# optimal translation, calculated via metric function
+	# the function is normalized by norm function around z
+	def trnsl(self, another, z, trim=0.1):
+		slen = len(self.data)
+		alen = len(another.data)
+		tlen = int(min(slen,alen)*trim) # trim length
+		sumlen = slen+alen
+		vals = np.zeros(sumlen-2*tlen)
+		for t in np.arange(tlen, sumlen-tlen):
+			init = max(t,alen)
+			end = min(t,slen)+alen
+			vals[t-tlen] = self.metric(another,init-alen,end-alen,init-t,end-t)
+		# optimal translation for another
+		am = int(np.argmin(vals))
+		dist = vals[am]
+		ot =  am + tlen - alen
+		return (z-ot, dist)
+
+	# measures the distance between two waves
+	# the greater, the more different are the waves
+	def metric(self, another, si, se, ai, ae):
+		num = se-si
+		if num != ae-ai:
+			raise NameError('need to be same length')
+		sumval = 0.0
+		for ind in range(num): 
+			sumval += abs(self.data[ind+si]-another.data[ai+ind])
+		return sumval / num
+
+	# perform gaussian normalization on self.data centered at z
+	# not used for now
+	def norm(self, z, std):
+		rf = norm(loc=z, scale=std)
+		result = self.data[:]
+		for ind in range(len(self.data)):
+			result[ind] *= rf.pdf(ind-z)
+		return wave(result)
+
+# core analyzing algorithms
+class analyzer:
+	def __init__(self, topimgs, botimgs):
+		self.topimgs = topimgs
+		self.botimgs = botimgs
+
+	@staticmethod
+	def pixavg (pic, x, y, size):
+		val = 0
+		num = 0
+		yi = -size
+		while yi <= size:
+			xs = size - abs(yi)
+			xi = -xs
+			while xi <= xs:
+				val += pic[y+yi,x+xi]
+				num += 1
+				xi += 1
+			yi += 1
+		return val/num
+		
+	@staticmethod
+	def tempseq (imgs, x, y):
+		seq = np.zeros(len(imgs))
+		for ind in range(len(imgs)):
+			seq[ind] = analyzer.pixavg(imgs[ind], x, y, 2)
+		return wave(seq)
+	
+	def trnsl (self, z): 
+		# given z in flt, select a few random points 
+		# with variance higher than given threshold
+		# assuming no translation between the two images
+		num = 20 # make 10 random selections
+		ots = np.zeros(num)
+		for ind in range(num):
+			ots[ind] = self.randtrnsl(z)
+		# print ots
+		return sp.stats.mode(ots)[0][0]
+	
+	# perform single random selection of point
+	# and return optimal translation
+	def randtrnsl(self, z, xi=100, xf=400, yi=100, yf=400):
+		x = int(round(xi + np.random.rand(1)[0]*(xf-xi)))
+		y = int(round(yi + np.random.rand(1)[0]*(yf-yi)))
+		w1 = analyzer.tempseq(self.topimgs, x, y) 
+		w2 = analyzer.tempseq(self.botimgs, x, y)
+		# print "(%d,%d)" %(x,y)
+		ot, dist = w1.trnsl(w2, z)
+		return ot
+
+# fetches all imeages from folderdir
+def fetcher(folderdir):
+	files = sorted(glob(os.path.join(folderdir, '*.jpg')))
+	topimgs = {} 
+	for ind in range(len(files)):
+		topimgs[ind] = mpimg.imread(files[ind])
+	return topimgs.values()
+
+# start application with given arguments
+# the first arg is flt, the second is ref
+topimgs = fetcher(sys.argv[1])
+botimgs = fetcher(sys.argv[2])
+d = drawer(topimgs, botimgs)
+
+"""
+job sequences
+0. solve the recursion max reach problem
+1. tester that sequentially perform tests on many data sets (mainly GUI based)
+2. automatically choose xi, xf, yi, yf in randtrnsl
+3. improve metric system between waves (localize)
+4. find way to match x,y on flt and ref, i.e. translation (***)
+	ideally this can be param'ized and machine learning can solve it
+5. matching when different scale (either x,y, or z)
+6. systemize testing and result recording
+7. implement machine learning
+
+"""
